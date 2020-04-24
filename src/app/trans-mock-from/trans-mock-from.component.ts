27e6cb07c80a9cbd34fc78bac5950f8e64708253
talshbt <talshbt@gmail.com>
2020-04-24 11:16:40 +0300

    1 file modified

--- src/app/trans-mock-from/trans-mock-from.component.ts
+++ src/app/trans-mock-from/trans-mock-from.component.ts
@@ -1,204 +1,114 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
-import { FormControl, FormGroup, FormArray, Validators } from '@angular/forms';
+import { FormControl, FormGroup, FormArray, Validators } from "@angular/forms";
 let emailRegex = "^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9-.]+$";
 @Component({
-  selector: 'app-trans-mock-from',
-  templateUrl: './trans-mock-from.component.html',
-  styleUrls: ['./trans-mock-from.component.css']
+  selector: "app-trans-mock-from",
+  templateUrl: "./trans-mock-from.component.html",
+  styleUrls: ["./trans-mock-from.component.css"]
 })
 export class TransMockFromComponent implements OnInit {
-treeArrObjects =[];
-childrenArrObjects = [];
-childrentObjects1 = {};
-constructor() { }
+  treeArrObjects = [];
+  childrenArrObjects = [];
+  childrentObjects1 = {};
+  constructor() {}
 
-form;
-transDict = 
-{
-  blockSend001Data: [],
-  anqnlc1wnigreretData: [
-    'sugNigreretInt', 
-    'sugPirteyMahaduraInt',
-
-  ],
-  blockReceive001Data: [],
-  anqnlc11nigreretData: [
-    'sugNigreretInt',
-
-  ],
-  anqtlc1wnigreretData: [
-    'sugNigreretInt',
-
-  ],
-  trqra001nigreretData: [],
-
-}
-
-//  arr =[] ;
+  form;
+  transDict = {
+    blockSend001Data: [],
+    anqnlc1wnigreretData: ["sugNigreretInt", "sugPirteyMahaduraInt"],
+    blockReceive001Data: [],
+    anqnlc11nigreretData: ["sugNigreretInt"],
+    anqtlc1wnigreretData: ["sugNigreretInt"],
+    trqra001nigreretData: []
+  };
 
 
   ngOnInit() {
-
-    this.createTreeArrObjects()
+    this.createTreeArrObjects();
 
     this.fillForm();
-  
-  
-  
   }
- 
 
-
-    createTreeArrObjects() {
-
-    var group = {}
+  createTreeArrObjects() {
+    var group = {};
     var counter = 0;
     for (var parent in this.transDict) {
       var formArr = [];
-      var parentObj = {}
-      parentObj['name'] = parent;
-       if (!this.hasChildren(parent)) {
-          formArr.push(new FormControl(parent))
-          parentObj['hasChild'] = false;
-
-
-       }
-
-      else  {
+      var parentObj = {};
+      parentObj["name"] = parent;
+      if (!this.hasChildren(parent)) {
+        formArr.push(new FormControl(parent));
+        parentObj["hasChild"] = false;
+      } else {
         var children = [];
-        var childObj = {}
-        parentObj['hasChild'] = true;
+        var childObj = {};
+        parentObj["hasChild"] = true;
         for (var child in this.transDict[parent]) {
-          formArr.push(new FormControl(this.transDict[parent][child]))
-          childObj[this.transDict[parent][child]] = this.transDict[parent][child];
-          children.push(this.transDict[parent][child])
+          formArr.push(new FormControl(this.transDict[parent][child]));
+          childObj[this.transDict[parent][child]] = this.transDict[parent][
+            child
+          ];
+          children.push(this.transDict[parent][child]);
         }
-        parentObj['children'] = children;
-        parentObj['childObj'] = childObj
-        
+        parentObj["children"] = children;
+        parentObj["childObj"] = childObj;
       }
-         
-        
-      
-        this.treeArrObjects.push(parentObj)
-     
-      
 
-
+      this.treeArrObjects.push(parentObj);
     }
-//  this.printTreeArr();
-   
-    // this.fillForm();
-  
-
-
-
   }
   hasChildren(parent) {
-    
     return this.transDict[parent].length > 0;
   }
 
-  createFormTemplate(){
-    var formGroup = {}
+  createFormTemplate() {
+    var formGroup = {};
     var arr = [];
-     var obj = {}
-     for(var x of this.treeArrObjects){
-      
-       obj[x.name] = new FormArray([]);
-
-
-     }
-    this.form = new FormGroup(obj)
-
-
-   
-
-
-  }
-
-
-
-
-
-  fillForm(){
-
-     this.createFormTemplate()
-      for(var item in this.treeArrObjects){
-         var obj = {};
-        if(!this.treeArrObjects[item]['hasChild']){
-         
-          obj[this.treeArrObjects[item]['name']] = this.treeArrObjects[item]['name']
-
-        }
-        else{
-        
-        var arr = this.treeArrObjects[item]['children'];
-       
-        for(var child in arr){
-          obj[arr[child]] = arr[child];
-       
-        }
-
-
-     }
-
-    let fg: any = {};
-
-    // console.log(obj)
-    this.childrentObjects1[this.treeArrObjects[item]['name']] = obj;
-    this.childrenArrObjects.push(obj)
-   
-
-    for (let control in obj) {
-
-      fg[control] = new FormControl(obj[control]);
-  
+    var obj = {};
+    for (var x of this.treeArrObjects) {
+      obj[x.name] = new FormArray([]);
     }
-    var name = this.treeArrObjects[item]['name'];
-    var fa = this.form.get(name) as FormArray
-   
-    fa.push(new FormGroup(fg))
-
-
-
-  
-
-  }
-      
-
+    this.form = new FormGroup(obj);
   }
 
+  fillForm() {
+    this.createFormTemplate();
+    for (var item in this.treeArrObjects) {
+      var obj = {};
+      if (!this.treeArrObjects[item]["hasChild"]) {
+        obj[this.treeArrObjects[item]["name"]] = this.treeArrObjects[item][
+          "name"
+        ];
+      } else {
+        var arr = this.treeArrObjects[item]["children"];
 
+        for (var child in arr) {
+          obj[arr[child]] = arr[child];
+        }
+      }
 
+      let fg: any = {};
 
+      // console.log(obj)
+      this.childrentObjects1[this.treeArrObjects[item]["name"]] = obj;
+      this.childrenArrObjects.push(obj);
 
+      for (let control in obj) {
+        fg[control] = new FormControl(obj[control]);
+      }
+      var name = this.treeArrObjects[item]["name"];
+      var fa = this.form.get(name) as FormArray;
 
+      fa.push(new FormGroup(fg));
+    }
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 //   printForm(){
 
 //      console.log(this.form)
-  
+
 //     // for(var i =0; i < Object.values(this.form.controls).length; ++i){
 //     //   console.log(Object.keys(this.form.controls)[i])
 //     //   var formArray:any = (Object.values(this.form.controls)[i])
@@ -212,13 +122,8 @@ transDict =
 // //  for(var item in this.form.controls){
 // //       console.log(Object.values(item))
 // //     }
- 
-   
 
 //   }
-
-
-
 
 //   printFormKeys(){
 //     Object.keys(this.form.controls).forEach(key=>{
@@ -226,28 +131,24 @@ transDict =
 //     })
 //   }
 
-
 //   printChildrenObjects(){
 //           for(var item in this.treeArrObjects){
 //             var keyName = this.treeArrObjects[item]['name'];
-       
 
 //             console.log(this.treeArrObjects[item]['name'] +":")
 
 //             var arr = Object.values(this.childrentObjects1[keyName])
 //             arr.forEach(value =>{
 //               console.log(value)
-            
+
 //             })
 
-            
 //           }
 
 //             console.log(this.form)
 
 //   }
 
-  
 //   printObj(obj){
 //       for(var key in obj){
 //        console.log("key :" + key)
@@ -270,5 +171,5 @@ transDict =
 
 //      }
 //      console.log(this.treeArrObjects)
-    
+
 //   }
