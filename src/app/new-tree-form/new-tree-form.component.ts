605ea3d4ba99fd144f171a0b7d90a2ae5b17c600
talshbt <talshbt@gmail.com>
2020-04-29 09:20:29 +0300

    1 file modified

--- src/app/new-tree-form/new-tree-form.component.ts
+++ src/app/new-tree-form/new-tree-form.component.ts
@@ -1,15 +1,17 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl, FormGroup, FormArray, Validators } from "@angular/forms";
 @Component({
   selector: 'app-new-tree-form',
   templateUrl: './new-tree-form.component.html',
   styleUrls: ['./new-tree-form.component.css']
 })
 export class NewTreeFormComponent implements OnInit {
-  treeMainDs = [];
-  transDictArr = [];
+;
   transDict2 = {};
-  form;
+
+  form2;
+
+  constructor() {}
 
   transDict = {
     blockSend001Data: [],
@@ -20,68 +22,101 @@ export class NewTreeFormComponent implements OnInit {
     trqra001nigreretData: []
   };
 
-    createTransDict2(){
-        for (var parent in this.transDict) {
-          var parentObj = {}
-          var key = parent;
-          var values = [];
-         
-          for(var child in this.transDict[parent]){
-            var childObj = {}
-            childObj[this.transDict[parent][child]] = ""
-            values.push(childObj)
-          }
-          parentObj[key] = values;
-          this.transDictArr.push(parentObj)
-          this.transDict2[key] = values;
 
-          
-        }
-
-        console.log(this.transDict2)
-
-
-        
-
-        // console.log(Object.keys(this.transDictArr[0])[0])
-        //  this.createTreeMainDs2()
-
-  }
-
-    createTreeMainDs() {
-    for (var parent in this.transDict) {
-      var parentObj = {};
-      parentObj["name"] = parent;
-      if (!this.hasChildren(parent)) {
-        parentObj["hasChild"] = false;
-      } else {
-        var childObj = {};
-        parentObj["hasChild"] = true;
-        for (var child in this.transDict[parent]) {
-          
-          childObj[this.transDict[parent][child]] = "";
-          
-        }
-
-         console.log(childObj)
-        parentObj["childObj"] = childObj;
-      }
-
-      this.treeMainDs.push(parentObj);
-    }
-
-   
-   
-
-  }
-  constructor() { }
 
   ngOnInit() {
+
     this.createTransDict2();
-    this.createTreeMainDs();
+    this.fillFormWithTreeDsData2();
+
+    var arr = this.form2.get("anqnlc1wnigreretData") as FormArray;
+
   }
 
-   hasChildren(parent) {
+
+
+
+  createTransDict2() {
+    for (var parent in this.transDict) {
+      var parentObj = {};
+      var key = parent;
+      var values = [];
+
+      for (var child in this.transDict[parent]) {
+        var childObj = {};
+        childObj[this.transDict[parent][child]] = "";
+        values.push(childObj);
+      }
+      parentObj[key] = values;
+      this.transDict2[key] = values;
+    }
+
+  }
+
+
+  createFormTemplate2() {
+    var templateFormobj = {};
+    for (var parent in this.transDict2) {
+      templateFormobj[parent] = new FormArray([]);
+    }
+    this.form2 = new FormGroup(templateFormobj);
+    console.log(this.form2);
+  }
+
+
+
+  fillFormWithTreeDsData2() {
+    this.createFormTemplate2();
+    for (var parent in this.transDict2) {
+      var controlObj = this.createControlObj2(parent);
+
+      let formGroup = this.addControlToFormControl2(controlObj);
+
+      var formArrayOfControls = this.getFormArray2(parent);
+
+      formArrayOfControls.push(new FormGroup(formGroup));
+    }
+  }
+
+
+  createControlObj2(parent) {
+    var controlObj = {};
+    if (!this.hasChildren(parent)) {
+      controlObj[parent] = parent;
+    } else {
+      for (var childObj in this.transDict2[parent]) {
+        var child = Object.keys(this.transDict2[parent][childObj])[0];
+
+        controlObj[child] = child;
+      }
+    }
+    return controlObj;
+  }
+
+ 
+
+  addControlToFormControl2(controlObj) {
+    let formGroup: any = {};
+
+    for (let control in controlObj) {
+      formGroup[control] = new FormControl(controlObj[control]);
+    }
+
+    return formGroup;
+  }
+
+
+
+  getFormArray2(parent) {
+    var controlName = parent;
+    return this.form2.get(controlName) as FormArray;
+  }
+
+  onSubmit() {
+    console.log(this.form2.value); // { cities: ['SF', 'NY'] }
+  }
+
+  hasChildren(parent) {
     return this.transDict[parent].length > 0;
   }
 
